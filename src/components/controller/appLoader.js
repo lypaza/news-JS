dc2eafb3c19f328f516b2f42128e1c0eb25d2aa9
German N <germannikitchenko@gmail.com>
2022-06-29 19:33:34 +0300

    refactor: preconfiguration for migrating to typescript(npm TypeScript package, ESLint, Webpack)

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '5d6f02d3aac742169ad63835cb2937fd', // получите свой ключ https://newsapi.org/
         });
     }
 }
